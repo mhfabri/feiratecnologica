0e60f13fa2a212b9bb893a874712f1f39de29829
mhfabri <matheusfabri337@gmail.com>
2025-10-21 16:54:25 -0300

    terminei

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 function compreaqui(){
     window.location.href = 'https://www.amazon.com.br/Filhos-do-Quarto-Elisabete-Baptista/dp/6584690075/ref=sr_1_1?dib=eyJ2IjoiMSJ9.Uieed_YXjYYRmqHh556o8CcXIMkpwB1vZbl7tj81nINVeiVjFBWq6HUoim1RUH0zJG0NeHi4TnGglYfTfbfOsjnH1y0vM3PRp70Qr79fPf9QuzJO-8oPu62fLC37ABzVGGDuLCd2FgZvzwDVnOiz1sEmnA81wm8tALoL4akmxGdcUmniE4wwz4jWrC1qfMWAkG2sqFPN6BPBxrpUjIUPnuYaRac3Ivlsl7jDIVy558JNza-HNyJ9_0g5DjbM_WJjngGTUcDbHmwYPOoKYQ6o7gTZZvBxwP6Fpjdsskj-EGY.qzy7LppNpNaAA5DzNjiH4LeDc77nZjzoPknumpBbD34&dib_tag=se&keywords=os+filhos+do+quarto&qid=1761045931&sr=8-1'
 }
+
+function cvv(){
+    window.location.href = 'https://www.cvv.org.br/'
+}
